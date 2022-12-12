ae97a590b4200efe4ee7ae0b865ac0d1042f1479
Veysel OZTURRK <veyselozturkeng@gmail.com>
2022-12-13 01:30:03 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello {
+
+    public static class Psrogram{
+
+        public static void Main(String[] Args){
+
+
+            Console.WriteLine("Veysel ÖZTÜRK.");
+        }
+    }
+}
+
